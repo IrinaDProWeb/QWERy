64ac5973b4ef3522d0f7b786a5ffa1ee042db0e8
IrinaDProWeb <98805290+IrinaDProWeb@users.noreply.github.com>
2022-02-01 19:30:53 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function(){
     $('.slider-body').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
-        prevArrow: "<img src='https://irinadproweb.github.io/QWERy/images/bromo-slider/prev-arrow.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='../images/bromo-slider/next-arrow.svg' class='next' alt='2'>",
+        prevArrow: "<img src='images/bromo-slider/prev-arrow.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='images/bromo-slider/next-arrow.svg' class='next' alt='2'>",
        });
 
     $('.menu-burger__header').click(function() {
