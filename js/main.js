39fb6ff05c053023022140eb349d0570e6148de0
IrinaDProWeb <98805290+IrinaDProWeb@users.noreply.github.com>
2022-02-02 21:58:30 +0700

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,25 @@
+$(function(){
+
+    $('.slider-body').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: "<img src='../images/bromo-slider/prev-arrow.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='../images/bromo-slider/next-arrow.svg' class='next' alt='2'>",
+
+        responsive: [
+            {
+              breakpoint: 767,
+              settings: {
+                arrows: false,
+              }
+            }
+        ]
+
+       });
+
+    $('.menu-burger__header').click(function() {
+        $('.menu-burger__header, .menu-list').toggleClass('open-menu');
+        $('body').toggleClass('fixed-page');
+      });
+
+});
